43605133b63722b69f9346e05e9119f8b9f8348e
Miracle Lawrence <lawrencemiracle71@gmail.com>
2022-08-22 15:26:51 +0100

    finished this game

--- main.py
+++ main.py
@@ -7,14 +7,15 @@ image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
 
-
 # read file
 data = pandas.read_csv("50_states.csv")
 all_states = data.state.to_list()
 
+not_guessed = []
 guessed_states = []
+gameon = True
 
-while len(guessed_states) < 50:
+while len(guessed_states) < 50 and gameon == True:
     answer_state = turtle.textinput(f"{len(guessed_states)}/50 States Guesses", "What's another state name?").title()
 
     if answer_state in all_states and answer_state not in guessed_states:
@@ -25,6 +26,17 @@ while len(guessed_states) < 50:
         t.goto(int(state_data.x), int(state_data.y))
         t.write(answer_state)
         guessed_states.append(answer_state)
+
+    elif answer_state == "Exit":
+        for states in all_states:
+            if states not in guessed_states:
+                not_guessed.append(states)
+        new_data = pandas.DataFrame(not_guessed)
+        new_data.to_csv("not-guesses.csv")
+        print(not_guessed)
+        gameon = False
+
+
 # convert from csv to Python list
 # create a new list
 # get the cordinates
